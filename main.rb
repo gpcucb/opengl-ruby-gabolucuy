ed3b205ee4bc2f65b1f6025ba119fcb87a902134
gabolucuy <gabolucuy@hotmail.com>
2017-04-25 20:29:17 -0400

    perspective view

--- main.rb
+++ main.rb
@@ -8,43 +8,55 @@ include Glut
 
 
 def display
+  glDepthFunc(GL_LEQUAL)
+  glEnable(GL_DEPTH_TEST)
+  glClearDepth(1.0)
   #Color de fondo negro
   glClearColor( 0.0, 0.0, 0.0, 0.0)
   #limpiamos la pantalla
-  glClear(GL_COLOR_BUFFER_BIT)
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
   #Modo Proyeccion
   glMatrixMode (GL_PROJECTION )
   #cargamos La matriz Identidad
   glLoadIdentity
   #Proyección ortográfica dentro del cubo señalado
-  glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0)
+  # glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0)
 
   #Modo Modelado
-  glMatrixMode(GL_MODELVIEW)
   #Dibujamos un triángulo
-  # glBegin(GL_TRIANGLES)
-  # glColor3f(1, 0, 0)
-  # glVertex3f(0.0, 0.8, 0.0)
-  # glColor3f(0.0, 1.0, 0)
-  # glVertex3f(-0.6, -0.2, 0.0)
-  # glColor3f(0, 0, 1.0)
-  # glVertex3f(0.6, -0.2, 0.0)
-  # glEnd
-  glBegin(GL_QUADS);
-  glColor3f(1, 0, 0)
-  glVertex3f(0.4, 0.4, 0.0);
-  glVertex3f(0.4, -0.4, 0.0);
-  glVertex3f(-0.4, -0.4, 0.0);
-  glVertex3f(-0.4, 0.4, 0.0);
- glEnd();
+
+   #  glBegin(GL_QUADS);
+   #  glColor3f(1, 0, 0)
+   #  glVertex3f(0.4, 0.4, 0.0);
+   #  glVertex3f(0.4, -0.4, 0.0);
+   #  glVertex3f(-0.4, -0.4, 0.0);
+   #  glVertex3f(-0.4, 0.4, 0.0);
+   # glEnd();
+   gluPerspective(60.0,1.0,1.0,100.0);
+   glTranslatef(0.0,0.0,-2.0)
+   glMatrixMode(GL_MODELVIEW)
+   
+   glBegin(GL_QUADS)
+      glColor3f(0.0,1.0,1.0)
+      glVertex3f(-0.5,0.5,-0.5)
+      glVertex3f(-0.5,-0.5,0.5)
+      glVertex3f(0.5,-0.5,0.5)
+      glVertex3f(0.5,0.5,-0.5)
+   glEnd()
+   glBegin(GL_TRIANGLES)
+      glColor3f(1, 0, 0)
+      glVertex3f(0.0, 0.5, 0.0)
+      glVertex3f(-0.7, -0.5, 0.0)
+      glVertex3f(0.7, -0.5, 0.0)
+   glEnd
   glFlush
+  # sleep(20)
   #para evitar que el ciclo siga corriendo
-  glutPostRedisplay
 end
 puts ("Hola Mundo!")
 
 glutInit
-glutInitDisplayMode( GLUT_SINGLE || GLUT_RGBA)
+glutInitDisplayMode( GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH)
 glutInitWindowSize(500,500)
 glutInitWindowPosition(20,20)
 glutCreateWindow("Hola Opengl en Ruby")
